40cb0931d5c1d0c49b10fa7fc598a3c71e75bf0f
Abhishek Tiwari <88926765+abhishektiwari98@users.noreply.github.com>
2021-09-27 15:11:28 -0700

    ThirtySorting

---
+++ src/main/java/indstate/ThirtySorting.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class ThirtySorting {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+        int totalSwaps = 0;
+        for (int i = 0; i < n; i++) {
+            // Track number of elements swapped during a single array traversal
+            int numberOfSwaps = 0;
+            
+            for (int j = 0; j < n - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a.get(j) > a.get(j + 1)) {
+                    swap(a, j, j+1);
+                    numberOfSwaps++;
+                }
+            }
+            
+            totalSwaps += numberOfSwaps;
+            
+            // If no elements were swapped during a traversal, array is sorted
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+
+        bufferedReader.close();
+        System.out.println("Array is sorted in " + totalSwaps + " swaps.");
+        System.out.println("First Element: " + a.get(0));
+        System.out.println("Last Element: " + a.get(a.size() - 1));
+    }
+    
+    private static void swap(List<Integer> input, int i, int j) {
+        int temp = input.get(i);
+        input.set(i, input.get(j));
+        input.set(j, temp);
+    }
+}
