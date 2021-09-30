3201ad3dacccff8cf10d9b98ebb5a1134b92ae1e
Abhishek Tiwari <abhishektiwari.b@gmail.com>
2021-09-30 09:37:42 +0530

    add package

--- src/main/java/indstate/ThirtySorting.java
+++ src/main/java/indstate/ThirtySorting.java
@@ -1,3 +1,5 @@
+package indstate;
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
@@ -9,8 +11,6 @@ import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
-
-
 
 public class ThirtySorting {
     public static void main(String[] args) throws IOException {
